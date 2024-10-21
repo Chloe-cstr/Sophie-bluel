332acb428867f32062a73abc6b47fdcef5c066b0
Chloe Casteran <chloe.casteran@gmail.com>
2024-10-21 17:30:42 +0200

    Récupération affichages des travaux de l'architecte

---
+++ FrontEnd/assets/js/works.js
@@ -0,0 +1,30 @@
+function recupWorks(){
+    fetch("http://localhost:5678/api/works")
+    .then(reponse => reponse.json())
+    .then(projects =>{
+        console.log(projects);
+        addWorks(projects);
+    })
+    .catch(error => console.error('Erreur lors de la récupération des projets:', error));
+}
+
+function addWorks(projects){
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = '';
+    projects.forEach(work=>{
+        const project = document.createElement("figure");
+        
+        const imageProject = document.createElement("img");
+        imageProject.src = work.imageUrl;
+        imageProject.alt = work.title;
+        project.appendChild(imageProject); //Ajout de l'image à la balise figure
+        
+        const titleProject = document.createElement("figcaption");
+        titleProject.textContent = work.title;
+        project.appendChild(titleProject); //Ajout du titre à la balise figure
+
+        gallery.appendChild(project); //Ajout de la balise figure à la div .gallery
+    })
+}
+
+recupWorks()
