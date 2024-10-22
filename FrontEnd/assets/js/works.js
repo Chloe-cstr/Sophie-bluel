4f33a04862950aa1b11936cdd7291464a23133b1
Chloe Casteran <chloe.casteran@gmail.com>
2024-10-22 18:48:12 +0200

    Ajout des filtres dynamiques (non fonctionnels)

--- FrontEnd/assets/js/works.js
+++ FrontEnd/assets/js/works.js
@@ -4,13 +4,16 @@ function recupWorks(){
     .then(projects =>{
         console.log(projects);
         addWorks(projects);
+        addFilters(projects)
     })
     .catch(error => console.error('Erreur lors de la récupération des projets:', error));
 }
 
 function addWorks(projects){
     const gallery = document.querySelector(".gallery");
+    
     gallery.innerHTML = '';
+    
     projects.forEach(work=>{
         const project = document.createElement("figure");
         
@@ -24,6 +27,32 @@ function addWorks(projects){
         project.appendChild(titleProject); //Ajout du titre à la balise figure
 
         gallery.appendChild(project); //Ajout de la balise figure à la div .gallery
+    })
+}
+
+function addFilters(projects){
+    const gallery = document.querySelector(".gallery");
+    const filters = document.querySelector(".filters");
+
+    //Récupère les id
+    const idCategory = new Set(projects.map(id=>id.category.id));
+    console.log(idCategory);
+
+    //Récupère les names
+    const nameCategory = new Set(projects.map(name=>name.category.name));
+    console.log(nameCategory);
+
+    //Créer les boutons
+    nameCategory.forEach(buttonFilter=>{
+        const filterContainer = document.createElement("div");
+        filterContainer.classList.add("filterContainer");
+    
+        const filter = document.createElement("h3");
+        filter.textContent = buttonFilter;
+        filter.classList.add("filterTitle");
+        filterContainer.appendChild(filter);
+
+        filters.appendChild(filterContainer);
     })
 }
 
